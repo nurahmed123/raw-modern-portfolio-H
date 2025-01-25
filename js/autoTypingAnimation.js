95ff5639ff97ee19580d06db436ff7efd583a9e0
Nur Ahmed <yeasiresha@gmail.com>
2025-01-25 18:52:11 +0600

    up

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Nur Ahmed. </span>I am a full-stack web developer. I've 2years+ experience. I've completed many projects over time. I am proficient in HTML, CSS, JavaScript, PHP jQuery, Bootstrap, and tailwind, NextJs, ReactJs. I challenge myself daily and learn new skills that helped me to do better work. here I provide quality and satisfy my clients. First of all, we'll discuss your project when I'm able to understand everything then we will start your project."
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
